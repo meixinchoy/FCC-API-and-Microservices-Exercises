9dfdba38bf6e122081589cab9ff774b8e720c559
meixin <meixinchoy@gmail.com>
2020-11-05 13:39:15 +0800

    completed body parser exercise

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
 app.use("/",
@@ -13,10 +13,8 @@ app.use("/",
   });
 
 // --> 11)  Mount the body-parser middleware  here
-app.use(function (req, res, next) {
-  bodyparser.urlencoded({ extended: false })
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
+console.log(bodyParser);
 
 /** 1) Meet the node console. */
 console.log("Hello World")
