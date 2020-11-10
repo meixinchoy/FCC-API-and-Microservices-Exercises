b05caf86dc466102a094caf158d0ec4e2bc8d363
meixin <meixinchoy@gmail.com>
2020-11-10 22:20:52 +0800

    finished mongodb and mongoose exercises

--- myApp.js
+++ myApp.js
@@ -54,7 +54,7 @@ let Person1 = Person({ name: "han", favoriteFoods: ["bruchetta", "carpeccio"] })
 // All the **CRUD** methods take a callback function like this as the last argument.
 
 var createAndSavePerson = function (done) {
-  var person1 = new Person({ name: "Janek", age: 64, favoriteFoods: ["egg", "bacon"] });
+  var person1 = new Person({ name: "Janek", age: 64, favoriteFoods: ["egg", "bacon", "burrito"] });
 
   person1.save(function (err, data) {
     if (err) return console.error(err);
@@ -69,7 +69,7 @@ var createAndSavePerson = function (done) {
 var arrayOfPeople = [
   { name: "Fran", age: 7, favoriteFoods: ["Chocolate"] },
   { name: "Ren", age: 6 },
-  { name: "Emmy", age: 8, favoriteFoods: ["Pizza"] }
+  { name: "Emmy", age: 8, favoriteFoods: ["Pizza", "burrito"] }
 ];
 
 var createManyPeople = function (arrayOfPeople, done) {
@@ -81,13 +81,12 @@ var createManyPeople = function (arrayOfPeople, done) {
 
 /** # C[R]UD part II - READ #
 /*  ========================= */
+// https://mongoosejs.com/docs/queries.html
 
 /** 5) Use `Model.find()` */
 
-// Find all the people having a given name, using `Model.find() -> [Person]`
-// In its simplest usage, `Model.find()` accepts a **query** document (a JSON
+// Find all that satisfies. `Model.find()` accepts a **query** document (a JSON
 // object ) as the first argument, and returns an **array** of matches.
-// It supports an extremely wide range of search options. Check it in the docs.
 let personName = "Ren";
 var findPeopleByName = function (personName, done) {
   Person.find({ name: personName }, function (err, personFound) {
@@ -99,8 +98,6 @@ var findPeopleByName = function (personName, done) {
 /** 6) Use `Model.findOne()` */
 
 // `Model.findOne()` behaves like `.find()`, but it returns **only one**
-// document, even if there are more. It is especially useful
-// when searching by properties that you have declared as unique.
 var findOneByFood = function (food, done) {
   Person.findOne({ favoriteFoods: food }, function (err, data) {
     if (err) return console.log(err);
@@ -110,9 +107,8 @@ var findOneByFood = function (food, done) {
 
 /** 7) Use `Model.findById()` */
 
-// When saving a document, mongodb automatically add the field `_id`,
-// and set it to a unique alphanumeric key.`moongose` provides a dedicated
-// method for it. Find the (only!!) person having a certain Id, using `Model.findById() -> Person`.
+// When saving a document, mongodb automatically add the field `_id`, and set
+// it to a unique alphanumeric key.`moongose` provides a dedicated method for it. 
 var findPersonById = function (personId, done) {
   Person.findById(personId, function (err, data) {
     if (err) return console.log(err);
@@ -122,6 +118,7 @@ var findPersonById = function (personId, done) {
 
 /** # CR[U]D part III - UPDATE # 
 /*  ============================ */
+// https://mongoosejs.com/docs/queries.html
 
 /** 8) Classic Update : Find, Edit then Save */
 
@@ -150,11 +147,6 @@ const findEditThenSave = (personId, done) => {
 };
 
 /** 9) New Update : Use `findOneAndUpdate()` */
-
-// Recent versions of `mongoose` have methods to simplify documents updating.
-// Some more advanced features (i.e. pre/post hooks, validation) beahve
-// differently with this approach, so the 'Classic' method is still useful in
-// many situations. `findByIdAndUpdate()` can be used when searching by Id.
 /*
 var findAndUpdate = function (personName, done) {
   var ageToSet = 20;
@@ -177,34 +169,30 @@ const findAndUpdate = (personName, done) => {
 
 /** # CRU[D] part IV - DELETE #
 /*  =========================== */
+// https://mongoosejs.com/docs/queries.html
 
 /** 10) Delete one Person */
 
 // Delete one person by her `_id`. You should use one of the methods
-// `findByIdAndRemove()` or `findOneAndRemove()`. They are similar to the
-// previous update methods. They pass the removed document to the cb.
-// As usual, use the function argument `personId` as search key.
-
+// `findByIdAndRemove()` or `findOneAndRemove()`, similar to the update methods
 var removeById = function (personId, done) {
-
-  done(null/*, data*/);
-
+  Person.findByIdAndRemove(personId, (err, removedPpl) => {
+    if (err) return console.log(err);
+    done(null, removedPpl);
+  })
 };
 
 /** 11) Delete many People */
 
 // `Model.remove()` is useful to delete all the documents matching given criteria.
-// Delete all the people whose name is "Mary", using `Model.remove()`.
-// Pass to it a query ducument with the "name" field set, and of course a callback.
-//
 // Note: `Model.remove()` doesn't return the removed document, but a document
 // containing the outcome of the operation, and the number of items affected.
-// Don't forget to pass it to the `done()` callback, since we use it in tests.
-
 var removeManyPeople = function (done) {
   var nameToRemove = "Mary";
-
-  done(null/*, data*/);
+  Person.remove({ name: nameToRemove }, function (err, personRemoved) {
+    if (err) return console.log(err);
+    done(null, personRemoved);
+  })
 };
 
 /** # C[R]UD part V -  More about Queries # 
@@ -215,25 +203,20 @@ var removeManyPeople = function (done) {
 // If you don't pass the `callback` as the last argument to `Model.find()`
 // (or to the other similar search methods introduced before), the query is
 // not executed, and can even be stored in a variable for later use.
-// This kind of object enables you to build up a query using chaining syntax.
-// The actual db search is executed when you finally chain
-// the method `.exec()`, passing your callback to it.
-// There are many query helpers, here we'll use the most 'famous' ones.
 
-// Find people who like "burrito". Sort them alphabetically by name,
-// Limit the results to two documents, and hide their age.
 // Chain `.find()`, `.sort()`, `.limit()`, `.select()`, and then `.exec()`,
 // passing the `done(err, data)` callback to it.
 
-var queryChain = function (done) {
-  var foodToSearch = "burrito";
+var foodToSearch = "burrito";
+Person.find({ favoriteFoods: foodToSearch })
+  .limit(2)
+  .select({ age: 0 })
+  .sort({ name: 1 })
+  .exec(function (err, pplFound) {
+    if (err) console.log(err);
+    console.log(pplFound);
+  })
 
-  done(null/*, data*/);
-};
-
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
 
 /** # Further Readings... #
 /*  ======================= */
@@ -246,7 +229,7 @@ var queryChain = function (done) {
 
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
-
+/*
 exports.PersonModel = Person;
 exports.createAndSavePerson = createAndSavePerson;
 exports.findPeopleByName = findPeopleByName;
@@ -258,7 +241,7 @@ exports.createManyPeople = createManyPeople;
 exports.removeById = removeById;
 exports.removeManyPeople = removeManyPeople;
 exports.queryChain = queryChain;
-
+*/
 
 
 
